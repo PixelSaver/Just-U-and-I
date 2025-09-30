81f30405adbfab40bebe621166eba7b176c43a30
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-09-29 18:45:02 -0700

    settings animations working

--- Scripts/button_settings.gd
+++ Scripts/button_settings.gd
@@ -4,13 +4,21 @@ class_name ButtonSettings
 signal self_pressed(val:ButtonSettings)
 signal hover(val:ButtonSettings)
 signal unhover(val:ButtonSettings)
+signal is_ready(val:ButtonSettings)
 @export var bg : Panel
 @export var label : RichTextLabel
+@export var margin_label : MarginContainer
+@export var tip_panel : Panel
 var og_pos : Vector2 = Vector2.ZERO
 var og_size : Vector2 
+var tip_pos : Vector2
 
 func _ready() -> void:
 	og_size = size
+	await get_tree().process_frame
+	tip_pos = tip_panel.global_position
+	tip_panel.modulate = Color(Color.WHITE, 0.5)
+	is_ready.emit(self)
 
 func _on_button_down() -> void:
 	self_pressed.emit(self)
--- Scripts/settings_menu.gd
+++ Scripts/settings_menu.gd
@@ -5,52 +5,55 @@ class_name SettingsMenu
 @export var button: ButtonSettings
 @export var offset : float = 50
 var buttons : Array[ButtonSettings] = []
+const TITLES = ["SOUND VOLUME", "AUDIO VOLUME", "FULLSCREEN", "HOW TO PLAY"]
+var duration : float = 0.2
 
 func _ready() -> void:
 	button.position = title.position + Vector2.ONE*200 + Vector2(0,15)
 	buttons.append(button)
+	button.label.text = TITLES[0]
 	button.og_pos = button.position
 	for i in range(3):
 		var inst = button.duplicate() as ButtonSettings
 		add_child(inst)
 		inst.position = button.position + Vector2.ONE*110*(i+1)
+		inst.label.text = TITLES[i+1]
 		inst.og_pos = inst.position
 		buttons.append(inst)
 	for _button in buttons:
 		_button.connect("self_pressed", _on_pressed)
 		_button.connect("hover", _on_hover)
 		_button.connect("unhover", _on_unhover)
+		_button.connect("is_ready", _on_button_ready)
 
 func _on_hover(but:ButtonSettings):
-	var call = Callable(set_skewed_height).bind(but)
-	var t = create_tween().set_trans(Tween.TRANS_QUINT).set_parallel(true).set_ease(Tween.EASE_OUT)
+	var t = create_tween().set_trans(Tween.TRANS_CUBIC).set_parallel(true)
 	
-	#t.tween_property(but.bg, "size:y", 100+offset, 0.5)
-	t.tween_property(but, "size:y", 125+offset, 0.5)
-	t.tween_property(but, "position", but.og_pos + Vector2(offset/2, 0), 0.5)
-	#t.tween_property(but, "size", but.og_size + Vector2.ONE*offset*2, 0.5)
+	t.tween_property(but, "size:y", 125+offset, duration)
+	t.tween_property(but, "position", but.og_pos + Vector2(offset/2, 0), duration)
+	t.tween_property(but.margin_label, "position:x", -offset/2, duration)
+	t.tween_property(but.tip_panel, "global_position", but.tip_pos, duration)
+	t.tween_property(but.tip_panel, "modulate", Color(Color.WHITE,1), duration)
+	t.tween_property(but.bg, "modulate", Color.WHITE, duration)
 	
 	for i in range(buttons.find(but)+1, buttons.size()):
-		t.tween_property(buttons[i], "position", buttons[i].og_pos + Vector2.ONE*offset, 0.5)
+		t.tween_property(buttons[i], "position", buttons[i].og_pos + Vector2.ONE*offset, duration)
 
 func _on_unhover(but:ButtonSettings):
-	var call = Callable(set_skewed_height).bind(but)
-	var t = create_tween().set_trans(Tween.TRANS_QUINT).set_parallel(true).set_ease(Tween.EASE_OUT)
+	var t = create_tween().set_trans(Tween.TRANS_CUBIC).set_parallel(true)
 	
-	#t.tween_property(but.bg, "size:y", 100, 0.5)
-	t.tween_property(but, "size:y", 125, 0.5)
-	t.tween_property(but, "position", but.og_pos, 0.5)
-	#t.tween_property(but, "size", but.og_size, 0.5)
+	t.tween_property(but, "size:y", 125, duration)
+	t.tween_property(but, "position", but.og_pos, duration)
+	t.tween_property(but.margin_label, "position:x", 0, duration)
+	t.tween_property(but.tip_panel, "global_position", but.tip_pos + Vector2(offset, offset), duration)
+	t.tween_property(but.tip_panel, "modulate", Color(Color.WHITE,.5), duration)
+	t.tween_property(but.bg, "modulate", Color.BLACK, duration)
 	
 	for i in range(buttons.find(but)+1, buttons.size()):
-		t.tween_property(buttons[i], "position", buttons[i].og_pos, 0.5)
+		t.tween_property(buttons[i], "position", buttons[i].og_pos, duration)
+
+func _on_button_ready(but:ButtonSettings):
+	but.tip_panel.global_position = but.tip_pos + Vector2(offset, offset)
 
 func _on_pressed(but:ButtonSettings):
 	pass
-
-func set_skewed_height(new_height: float, control:ButtonSettings) -> void:
-	var skew = -1.0  # your shear factor
-	var old_height = control.bg.size.y
-	control.bg.size.y = new_height
-	
-	control.position.x = control.og_pos.x + (new_height-100)/2
