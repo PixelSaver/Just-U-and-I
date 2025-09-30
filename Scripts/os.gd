d5252e48d1e630ad9d9c81871ca83481db0b1670
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-09-30 13:23:39 -0700

    cleaned up code for anim

--- Scripts/os.gd
+++ Scripts/os.gd
@@ -12,38 +12,49 @@ var touch = false
 var rect = []
 var pos = []
 var spr_pos = []
-
 var time_since_start = 0.0
+var animated_programs = [] 
+var is_animating = false
 
 func _ready() -> void:
-	print("Programs found: ", programs.size())
+	load_programs()
+
+func load_programs():
+	if is_animating: 
+		return
+	is_animating = true
+	animated_programs.clear() 
+	time_since_start = 0.0
 	
-	for prog in programs:
-		var i = prog as Program
-		pos.append(i.position)
-		rect.append(i.size)
-		spr_pos.append(i.program_sprite.position)
+	if pos.is_empty():
+		for prog in programs:
+			var i = prog as Program
+			pos.append(i.position)
+			rect.append(i.size)
+			spr_pos.append(i.program_sprite.position)
 	
-	print("Stored positions: ", pos.size())
-		
 	for i in range(programs.size()):
 		var p = programs[i] as Program
+		var f = p.get_node_or_null("flash")
+		if f:
+			f.queue_free()
+		
+		p.program_label.modulate.a = 1
+		p.position = pos[i] + Vector2(1000 + rect[i].x, rect[i].y / 2)
+		p.size = Vector2.ZERO
+		p.hide()
+		
 		var flash = ColorRect.new()
-		p.spin_speed = [-0.1, 0.1][randi() % 2]
+		flash.name = "flash"
 		flash.mouse_filter = Control.MOUSE_FILTER_IGNORE
-		flash.size = Vector2.ONE * (p.size.length() * ((i + 1) * 2))
+		flash.size = Vector2.ONE * (rect[i].length() * ((i + 1) * 2))
 		flash.position = Vector2(size.y / 4, -size.y / 3)
 		flash.rotation = 45
 		flash.show_behind_parent = true
-		flash.name = "flash"
 		p.add_child(flash)
-		p.program_label.modulate = Color(0, 0, 0, 5555)
-		p.position.x += 1000 + p.size.x 
-		p.position.y += p.size.y / 2
-		p.size = Vector2.ZERO
+		p.spin_speed = [-0.1, 0.1][randi() % 2]
 
 func _input(event: InputEvent) -> void:
-	# Handle touch input
 	if event is InputEventScreenTouch:
 		touch = event.pressed
 		if event.pressed:
@@ -65,28 +76,28 @@ func _physics_process(delta: float) -> void:
 		var last_program = programs.back() as Program
 		scroll += (clamp(scroll, 0, last_program.position.x + last_program.size.x - 450) - scroll) * 0.3
 	
-	#if Input.is_action_just_pressed("click_left") or Input.is_action_just_pressed("ui_select"):
-		#last_ms_pos = get_local_mouse_position().x
-		#last_scroll = scroll
-	
-	#if Input.is_action_pressed("click_left") or touch:
-		#if touch:
-			#scroll = last_scroll - (get_local_mouse_position().x - last_ms_pos) * 3
-		#else:
-			#scroll = last_scroll - (get_local_mouse_position().x - last_ms_pos)
+	if pos.is_empty(): 
+		return
 	
 	for i in range(programs.size()):
+		if i >= int(time_since_start) or i in animated_programs:
+			continue
+			
 		var p = programs[i] as Program
+		p.show()
 		
-		if i < int(time_since_start):
-			p.show()
-
-			var t := create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT).set_parallel(true)
-			
-			t.tween_property(p, "position", pos[i], 0.3)
-			t.tween_property(p, "size", rect[i], 0.3)
-			
-			# Shine / Flash anim
-			var flash := p.get_node_or_null("flash")
-			if flash:
-				t.tween_property(flash, "size:x", 0, 0.5).set_trans(Tween.TRANS_BACK)
+		var t := create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT).set_parallel(true)
+		t.tween_property(p, "position", pos[i], 0.3)
+		t.tween_property(p, "size", rect[i], 0.3)
+		
+		var flash := p.get_node_or_null("flash")
+		if flash:
+			var flash_tween = t.tween_property(flash, "size:x", 0, 0.5).set_trans(Tween.TRANS_BACK)
+			flash_tween.finished.connect(func(): 
+				if flash and is_instance_valid(flash):
+					flash.queue_free()
+					if i == programs.size() - 1:
+						is_animating = false
+			)
+		
+		animated_programs.append(i)
