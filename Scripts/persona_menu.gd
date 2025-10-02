15d4b6708c031b7021db99dc3f0171f880e7c3d2
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-10-02 14:48:42 -0700

    added circle to selector thingy

--- Scripts/persona_menu.gd
+++ Scripts/persona_menu.gd
@@ -7,8 +7,14 @@ func _ready() -> void:
 	selector.modulate.a = 0
 
 var selector_dur : float = 0.6
+var s : Tween
 func update_selector(node:Control):
-	var s = create_tween().set_trans(Tween.TRANS_QUINT).set_parallel(true)
+	if s: s.kill()
+	s = create_tween().set_trans(Tween.TRANS_QUINT).set_parallel(true)
 	s.set_ease(Tween.EASE_OUT)
 	s.tween_property(selector, "modulate:a", 1, selector_dur)
 	s.tween_property(selector, "global_position", node.global_position + node.pivot_offset, selector_dur)
+	
+	selector.circle.scale = Vector2.ONE * 0.5
+	s.set_ease(Tween.EASE_OUT)
+	s.tween_property(selector.circle, "scale", Vector2.ONE*2.5*node.scale, selector_dur)
--- Scripts/persona_selector.gd
+++ Scripts/persona_selector.gd
@@ -3,7 +3,16 @@ class_name PersonaSelector
 
 @export var x : Control
 @export var y : Control
+@export var circle : Control
 
 func _ready():
 	x.pivot_offset = x.size / 2
 	y.pivot_offset = y.size / 2
+	circle.pivot_offset = circle.size / 2
+
+func update_width(new_width:float):
+	x.size.y = new_width
+	y.size.x = new_width
+	x.pivot_offset = x.size / 2
+	y.pivot_offset = y.size / 2
+	
