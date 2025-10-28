e977ca59a3d8821b91cf61d85b1af20959d9d603
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-10-28 10:23:39 -0700

    Made anim script to string it all together :D

--- Scenes/GlitchMenu/glitch_menu.gd
+++ Scenes/GlitchMenu/glitch_menu.gd
@@ -1,11 +1,39 @@
 extends Control
 
+signal anim_finished
+@export var glitch_effect : ColorRect
+@export var pixel_sort : ColorRect
+@export var glitch_transition : ColorRect
+var t : Tween
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	glitch_effect_t(0)
+	pixel_sort_t(0)
+	glitch_trans_t(0)
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("click_left"):
+		anim()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func anim():
+	if t: t.kill()
+	
+	t = create_tween().set_ease(Tween.EASE_IN)
+	t.set_parallel(true).set_trans(Tween.TRANS_QUINT)
+	
+	t.tween_method(glitch_effect_t, 0., 1., 0.5)
+	t.tween_method(pixel_sort_t, 0., 1., 0.5)
+	t.tween_method(glitch_trans_t, 0., 1., 0.5)
+	
+	await t.finished
+	anim_finished.emit()
+
+func glitch_effect_t(val:float):
+	var shader_mat = glitch_effect.material as ShaderMaterial
+	shader_mat.set_shader_parameter("shake_rate", val)
+func pixel_sort_t(val:float):
+	var shader_mat = pixel_sort.material as ShaderMaterial
+	shader_mat.set_shader_parameter("sort", val)
+func glitch_trans_t(val:float):
+	var shader_mat = glitch_transition.material as ShaderMaterial
+	shader_mat.set_shader_parameter("fade", val)
