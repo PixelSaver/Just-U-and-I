eea5ba6453061ff909d9e025f07725e5db2adfdf
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-10-30 20:03:57 -0700

    Eye follows keyboard focused ui or mouse position depending on if the last input was a mouse movement or a keyboard press

--- Scenes/GlitchMenu/eye.gd
+++ Scenes/GlitchMenu/eye.gd
@@ -11,9 +11,16 @@ class_name Eye
 #@export var white_mult : Vector2 = Vector2(0.3, 0.1)
 @export_range(1., 10.) var pupil_speed : float = 1
 var eye_target : Vector2 = Vector2.ZERO
+var is_mouse : bool = true
 
 func _ready() -> void:
 	blood_anim.play("blank")
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		is_mouse = false
+	elif event is InputEventMouseMotion:
+		is_mouse = true
 
 func _process(delta: float) -> void:
 	eye_target = get_viewport().get_mouse_position()
@@ -24,7 +31,13 @@ func _process(delta: float) -> void:
 
 func _update_pupil(delta: float):
 	var eye_center = pupil.get_parent().global_position
-	var local_mouse = (eye_target - eye_center) * pupil_mult
+	var local_target : Vector2
+	if not is_mouse and get_viewport().gui_get_focus_owner():
+		var cont = get_viewport().gui_get_focus_owner()
+		var cont_pos = cont.global_position + cont.size/2
+		local_target = (cont_pos - eye_center) * pupil_mult
+	else:
+		local_target = (eye_target - eye_center) * pupil_mult
 
 	## Width
 	var a = 350
@@ -32,11 +45,11 @@ func _update_pupil(delta: float):
 	var b = 100
 
 	# Clamp inside ellipse
-	var ratio = sqrt((local_mouse.x*local_mouse.x)/(a*a) + (local_mouse.y*local_mouse.y)/(b*b))
+	var ratio = sqrt((local_target.x*local_target.x)/(a*a) + (local_target.y*local_target.y)/(b*b))
 	if ratio > 1:
-		local_mouse /= ratio
+		local_target /= ratio
 
-	var target = eye_center + local_mouse
+	var target = eye_center + local_target
 	var lerp_val = pupil.global_position.slerp(target, delta * pupil_speed)
 
 	pupil.global_position = lerp_val
